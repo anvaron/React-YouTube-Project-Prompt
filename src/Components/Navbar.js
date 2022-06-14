8f6c71fb30a6d9221c05b0cc2adb04f11649bed4
Andres Varon <anvaron@gmail.com>
2022-06-14 02:27:55 -0500

    Generating Components

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import { link } from "react/"
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="Navbar">
+        <div className="">
+          menu  
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
